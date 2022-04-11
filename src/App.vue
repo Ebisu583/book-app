1ba09af601c6a097d63be88b35186618e8ee917a
Radoslaw Tyszkiewicz <ebisu.poland@gmail.com>
2022-04-11 18:43:16 +0900

    Created book-app with add and remove functionality

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">
+
+    <!-- heading -->
+    <header>
+      <h1>Books<span>.app</span></h1>
+    </header>
+
+    <!-- books list -->
+    <ul>
+     <li
+     :key="index"
+     v-for="(book, index) in books">
+       {{ book.title }}, {{ book.price }}$
+       <button @click="removeBook(index)">Remove</button>
+     </li>
+    </ul>
+
+    <!-- no books message -->
+    <p v-if="books.length > 5">{{ books.length }} books</p>
+    <p v-else-if="books.length > 1">Not too many of them</p>
+    <p v-else-if="books.length === 1">One single book</p>
+    <p v-else>Go get some books</p>
+
+    <!-- add book form -->
+    <form>
+      <label>
+        Title:
+         <input v-model="form.title" type="text" name="title">
+      </label>
+      <label>
+        price:
+       <input v-model="form.price" type="number" name="price">
+      </label>
+      <button @click.prevent="handleSubmit">Add book</button>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    books: [
+      {
+        title: 'The Catcher in the Rye',
+        price: 20
+      },
+      {
+        title: 'Of Mice and Men',
+        price: 18
+      }
+    ],
+    form: {
+      title: '',
+      price: 0
+    }
+  }),
+  methods: {
+    removeBook (index) {
+      this.books.splice(index, 1)
+    },
+    handleSubmit () {
+      this.books.push({ ...this.form })
+      this.form.title = ''
+      this.form.price = 0
+    }
+  }
+}
+</script>
